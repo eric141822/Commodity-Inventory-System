5c9be97200258ac4359391f2c4fe512c5fa132ba
timmyhsu2 <47243108+timmyhsu2@users.noreply.github.com>
2019-04-26 13:52:56 +0800

    Add function new_entry

---
+++ new_entry.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include "functions.h"
+#include <iostream>
+using namespace std;
+
+void new_entry(vector<item> &items){
+  cout<<"Please type in the form of:"<<'\n';
+  cout<<"name availability amount price retailer"<<'\n';
+  item temp;
+  bool new_item=true;
+  cin>>temp.name>>temp.flag>>temp.stock>>temp.price>>temp.shops;
+  temp.soldout=0;
+  for (int i = 0; i<items.size(); i++){
+    if (items[i].name==temp.name&&items[i].shops==temp.shops){
+      //replace the original with temp
+      items[i]=temp;
+      new_item=false;//it's an existing item just updated
+    }
+  }
+  if (new_item==true){
+    items.push_back(temp);
+  }
+}
