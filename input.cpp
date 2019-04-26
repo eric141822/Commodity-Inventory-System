3997d7ca103ba223d96599a6f0e4086a0183ae50
eric141822 <47049224+eric141822@users.noreply.github.com>
2019-04-26 20:08:35 +0800

    Updated in-code documentations.

--- input.cpp
+++ input.cpp
@@ -5,12 +5,13 @@
 #include <vector>
 #include "functions.h"
 using namespace std;
+//This function reads a string (which is the file name of the list) as an input, and stores the info into a vector.
 void get_item_list (string filename, vector<item> &items)
 {
   ifstream fin;
   fin.open(filename);
   string line;
-  if (fin.fail()){
+  if (fin.fail()){     //Force quit the program if it fails.
     cout<<"Failed to open "<<filename<<"\nShutting system down.\n";
     exit(1);
   }
@@ -18,7 +19,7 @@ void get_item_list (string filename, vector<item> &items)
   int stock, i = 0, soldout;
   double price;
   item temp;
-  while (fin>>name>>flag>>stock>>price>>shops>>soldout){
+  while (fin>>name>>flag>>stock>>price>>shops>>soldout){. //Read until eof.
     temp.name = name;
     temp.flag = flag;
     temp.stock = stock;
