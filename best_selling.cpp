894e5e3ed632c26ad7ced664d052c8a195f10359
eric141822 <47049224+eric141822@users.noreply.github.com>
2019-04-26 20:05:20 +0800

    Updated in-code documentation.

--- best_selling.cpp
+++ best_selling.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 void policy (vector<item> &item)
 {
-  int first = 0, second = 0, third = 0;
-  int option;
+  int first = 0, second = 0, third = 0;    //This fisrt part finds the most popular items.
+  int option;                              //By sorting the "soldout" part of the struct. 
   for (int j = 0; j<item.size(); j++){
     if (item[j].soldout > first){
       third = second;
@@ -23,14 +23,14 @@ void policy (vector<item> &item)
       third = item[j].soldout;
     }
   }
-  if (first == second){
+  if (first == second){                     //This is to avoid reapeating output in case first = second, or second = third.
     second--;
   }
-  if (second == third){
+  if (second == third){ 
     third--;
   }
-  for (int i = 0; i<item.size(); i++){
-    if (item[i].soldout == first){
+  for (int i = 0; i<item.size(); i++){     //We then list out the top selling items.
+    if (item[i].soldout == first){.        //Note that there may be 2 No.1 best selling items.
       cout<<"No.1 Best-selling item: "<<item[i].name<<'\n';
     }
   }
@@ -43,7 +43,7 @@ void policy (vector<item> &item)
     if (item[i].soldout == third){
       cout<<"No.3 Best-selling item: "<<item[i].name<<'\n';
     }
-  }
+  }                                       //Ask the user which "policy" to apply.
   cout<<"Available policies:\n1. 20% off for all items.\n2. Buy 3 get 1 free.\n(Note that you can only have one policy at a time)\nPlease enter your option: ";
   cin>>option;
   switch (option){
